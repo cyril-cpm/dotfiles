a784720119d08fca7210a876a762328b4b98c435
cpm <cpm.cyril@gmail.com>
2026-01-11 00:29:53 +0100

    autoUpdateDotfiles

---
+++ tmux/.config/tmux/___back/src/palette_cybrland_dark.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+##########################
+# cybrland dark pallete
+##########################
+
+####
+# When using 'colour124' you are using the color default in terminal pallete.
+# This could be important for people which terminals support only 256 colors
+# and does not support HEX values.
+#
+# The names of colors used from https://github.com/morhetz/cybrland
+
+# shellcheck disable=2034 # ignored as this file only contains var definitions
+col_bg="#030408"
+col_bg0_h="#030408"
+col_bg0="#030408"
+col_bg1="#030408"
+col_bg2="#030408"
+col_bg3="#030408"
+col_bg4="#030408"
+col_gray0="#030408"
+col_gray1="#030408"
+col_gray2="#030408"
+col_bg0_s="#030408"
+col_fg="#F24848"
+col_fg4="#F24848"
+col_fg3="#F24848"
+col_fg2="#F24848"
+col_fg1="#F24848"
+col_fg0="#F24848"
+
+col_red="#F24848"
+col_red2="#F24848"
+col_green="#30F291"
+col_green2="#30F291"
+col_yellow="#F2D230"
+col_yellow2="#F2D230"
+col_blue="#3061F2"
+col_blue2="#3061F2"
+col_purple="#A130F2"
+col_purple2="#A130F2"
+col_aqua="#29BECC"
+col_aqua2="#29BECC"
+col_orange="#F26118"
+col_orange2="#F26118"
